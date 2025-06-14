c72f1e2aaac204f04498827b224545fb0e4d808a
entrpyc <asenangelov.cv@gmail.com>
2025-06-14 20:55:15 +0300

    add api route

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,4 +1,21 @@
+'use client'
+
+import { useEffect } from "react";
+
 export default function AnalyticsPage() {
+
+  const fetchAnalytics = async() => {
+    const res = await fetch('/api/analytics');
+    const data = await res.json();
+
+    console.log(data)
+    return data;
+  }
+
+  useEffect(() => {
+    fetchAnalytics();
+  }, [])
+  
   return (
     <p>AnalyticsPage</p>
   )
